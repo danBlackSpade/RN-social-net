9101ff53412355790b459f835f97de7ee2256914
Daniel J <djun44@gmail.com>
2023-11-22 21:04:22 -0300

    improvements on design BottomTab

--- screens/BottomTabs.js
+++ screens/BottomTabs.js
@@ -4,6 +4,8 @@ import { createMaterialBottomTabNavigator } from '@react-navigation/material-bot
 import { useTheme, Portal, FAB } from 'react-native-paper';
 import { useIsFocused, RouteProp } from '@react-navigation/native';
 import {StyleSheet, Text, SafeAreaView} from 'react-native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
 
 import Overlay from '../utils/Overlay';
 import { Feed } from './Feed';
@@ -48,38 +50,49 @@ return (
     <React.Fragment>
         
     <Tab.Navigator
-    barStyle={{ backgroundColor: theme.colors.background }}
+        barStyle={{ backgroundColor: theme.colors.background }}
         initialRouteName="Feed"
         backBehavior="initialRoute"
-        shifting={true}
-        activeColor={theme.colors.outline}
-        inactiveColor={color(theme.colors.onSurface)
-        .alpha(0.6)
-        .rgb()
-        .string()}
+        shifting={false}
+        activeColor={theme.colors.secondary}
+        inactiveColor={color(theme.colors.onBackground)
+            .alpha(0.25)
+            .rgb()
+            .string()}
+        
+        labeled={true}
+        
         sceneAnimationEnabled={false}
     >
         <Tab.Screen
+        
         name="Feed"
         component={Feed}
         options={{
             tabBarIcon: 'home-account',
             tabBarColor,
+
+            
         }}
         />
         <Tab.Screen
+        tabBarLabel={{ color: 'red' }}
         name="Notifications"
         component={Feed}
         options={{
             tabBarIcon: 'bell-outline',
             tabBarColor,
+            
         }}
         />
         <Tab.Screen
-        name="Messages"
+        name="Bookmarks"
         component={Details}
         options={{
-            tabBarIcon: 'message-text-outline',
+            tabBarIcon: ({ color }) => (
+                <MaterialCommunityIcons name='bookmark-outline' color={color} size={26}/>
+            ),
+
             tabBarColor,
         }}
         />
