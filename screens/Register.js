9b7d94f19183de5ea9281e4f0454d10683b4dbc7
Daniel J <djun44@gmail.com>
2023-12-01 08:33:27 -0300

    fixed keyboardAvoidingView on Register screen

--- screens/Register.js
+++ screens/Register.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { View, Text, Touchable, TouchableOpacity } from 'react-native';
+import { View, Text, Touchable, TouchableOpacity, StyleSheet, Keyboard } from 'react-native';
 
 import { KeyboardAvoidingView } from 'react-native';
 import { useTheme } from 'react-native-paper';
@@ -18,11 +18,66 @@ export const Register = ({ navigation }) => {
     const [username, onChangeUsername] = React.useState('');
     const [visible, setVisible] = React.useState('none');
     const [name, onChangeName] = React.useState(''); 
-    
-        return (
-            <Background>
-                <Header>Crie sua conta</Header>
+    const [isKeyboardVisible, setKeyboardVisible] = React.useState(false);
+    const [headerVisible, setHeaderVisible] = React.useState('flex');
 
+    React.useEffect(() => {
+        const keyboardDidShowListener = Keyboard.addListener('keyboardDidShow', () => { setKeyboardVisible(true); setHeaderVisible('none');  });
+        const keyboardDidHideListener = Keyboard.addListener('keyboardDidHide', () => { setKeyboardVisible(false); setHeaderVisible('flex'); });
+        console.log('useEffect: Keyboard Check: ' + headerVisible + ' ' + isKeyboardVisible);
+        // cleanup function
+        return () => {
+            keyboardDidHideListener.remove();
+            keyboardDidShowListener.remove();	
+            // Keyboard.removeListener('keyboardDidShow', _keyboardDidShow);
+            // Keyboard.removeListener('keyboardDidHide', _keyboardDidHide);
+        };
+        
+    }, []);
+    
+    async function sendRegister(navigation) {
+        let response = await fetch('http://192.168.1.130:3000/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ email: email, password: password, username: username, name: name }),
+        });
+        let json = await response.json();
+        if (json === 'error') {
+            setVisible('flex');
+            setTimeout(() => {
+                setVisible('none');
+            }, 10000);
+        } else {
+            await AsyncStorage.setItem('userData', JSON.stringify(json));
+            let resData = await AsyncStorage.getItem('userData');
+            json = JSON.parse(resData);
+            setCurrentUser({'username': json.username, 'email': json.email, 'name': json.name, 'isLoggedIn': true});
+            console.log('User registered successfully : ' + json.email);
+            navigation.navigate('Feed');
+        }
+    }
+
+        return (
+            <View style={{
+                flex: 1,
+                padding: 10,
+                width: '100%',
+                maxWidth: 340,
+                alignSelf: 'center',
+                alignItems: 'center',
+                justifyContent: 'center',
+            }}>
+                {/* <Text >Logo</Text> */}
+                <Background>
+                <View 
+                // style={{display: headerVisible}}
+                >
+                    <Header >Crie sua conta
+                    </Header>
+                </View>
+                
                 <TextInput 
                     label='Nome'
                     returnKeyType='next'
@@ -65,7 +120,7 @@ export const Register = ({ navigation }) => {
                 />
 
                 <Button
-                    onPress={() => console.log('register')}
+                    onPress={() => sendRegister(navigation) }
                     style={{ marginTop: 24 }}
                 >
                 REGISTRAR    
@@ -80,5 +135,15 @@ export const Register = ({ navigation }) => {
                 </View>
 
             </Background>
+            </View>
         );
 }
+
+const styles = StyleSheet.create({ 
+    header: (text="flex") => ({
+        display: text,
+    }),
+
+    
+
+});
